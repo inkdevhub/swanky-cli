ab8f4d7e8948f32894a21e3542c41489e16a79ba
codespool <4625220+codespool@users.noreply.github.com>
2022-05-28 23:31:08 +0300

    move all tasks to listr2

--- src/commands/init/index.ts
+++ src/commands/init/index.ts
@@ -2,14 +2,20 @@ import { execSync } from "node:child_process";
 import { Command, Flags } from "@oclif/core";
 import * as path from "node:path";
 import { rmSync, createWriteStream, existsSync, mkdirSync } from "node:fs";
-import * as inquirer from "inquirer";
 import { Listr } from "listr2";
-// import * as decompress from "decompress";
+import * as decompress from "decompress";
 import * as download from "download";
-import * as ProgressBar from "progress";
 import { nodes } from "../../nodes";
 
-interface Ctx {}
+interface Ctx {
+  platform: string;
+  contractTemplate?: string;
+  name: string;
+  nodeType?: string;
+  nodeUrl?: string;
+  nodeTargetDir?: string;
+  nodeFileName?: string;
+}
 export class Generate extends Command {
   static description = "Generate a new smart contract environment";
 
@@ -36,27 +42,150 @@ export class Generate extends Command {
       this.error(`Sorry, ${flags.language} is not supported yet`, { exit: 0 });
     }
 
-    const tasks = new Listr<Ctx>([checkDependencies], {
-      rendererOptions: { collapse: false },
-    });
+    const tasks = new Listr<Ctx>(
+      [
+        {
+          title: "Checking dependencies",
+          task: (ctx, task) => {
+            const tasks = Object.entries({
+              rust: "rustc --version",
+              cargo: "cargo -V",
+              "cargo contract": "cargo contract -V",
+            }).map(([dependency, command]) => ({
+              title: `Checking ${dependency}`,
+              task: () => {
+                try {
+                  execSync(command, { stdio: "ignore" });
+                } catch {
+                  throw new Error(
+                    `"${dependency}" is not installed. Please follow the guide: https://docs.substrate.io/tutorials/v3/ink-workshop/pt1/#update-your-rust-environment`
+                  );
+                }
+              },
+            }));
+            return task.newListr(tasks, { concurrent: true });
+          },
+        },
+        {
+          title: "Pick template",
+          task: async (ctx, task): Promise<void> => {
+            const template = await task.prompt([
+              {
+                name: "contractTemplate",
+                message: "Which template should we use?",
+                type: "Select",
+                choices: [
+                  { message: "Blank", name: "master" },
+                  { message: "Flipper", name: "flipper" },
+                  { message: "Dual contract", name: "dual-contract" },
+                ],
+              },
+            ]);
+            ctx.contractTemplate = template;
+          },
+        },
+        {
+          title: "Cloning template repo",
+          task: (ctx): void => {
+            execSync(
+              `git clone -b ${
+                ctx.contractTemplate
+              } --single-branch https://github.com/AstarNetwork/swanky-template-ink.git "${path.resolve(
+                ctx.name
+              )}"`,
+              { stdio: "ignore" }
+            );
+            rmSync(`${path.resolve(ctx.name, ".git")}`, {
+              recursive: true,
+            });
+            execSync(`rm -f ${ctx.name}/**/.gitkeep`, { stdio: "ignore" });
+            execSync(`git init`, { cwd: ctx.name });
+          },
+        },
+        {
+          title: "Pick node type",
+          task: async (ctx, task): Promise<void> => {
+            const nodeType = await task.prompt([
+              {
+                name: "nodeType",
+                message: "What node type you want to develop on?",
+                type: "Select",
+                choices: [{ message: "Swanky node", name: "swanky" }],
+              },
+            ]);
+            const targetDir = path.resolve(ctx.name, "bin");
+            if (!existsSync(targetDir)) {
+              mkdirSync(targetDir);
+            }
+
+            ctx.nodeType = nodeType;
+            ctx.nodeUrl = nodes[nodeType][ctx.platform];
+            ctx.nodeTargetDir = targetDir;
+            ctx.nodeFileName = `${nodeType}-node`;
+          },
+        },
+        {
+          title: "Downloading node",
+          task: async (ctx, task): Promise<void> =>
+            new Promise<void>((resolve, reject) => {
+              const writer = createWriteStream(
+                path.resolve(ctx.nodeTargetDir as string, "node.zip")
+              );
+
+              const response = download(ctx.nodeUrl as string);
+
+              response.on("response", (res) => {
+                const contentLength = Number.parseInt(
+                  res.headers["content-length"] as unknown as string,
+                  10
+                );
+                let progress = 0;
+                response.on("data", (chunk) => {
+                  progress += chunk.length;
+                  task.output = `Downloaded ${(
+                    (progress / contentLength) *
+                    100
+                  ).toFixed(0)}%`;
+                });
+                response.on("end", () => {
+                  resolve();
+                });
+                response.on("error", (error) => {
+                  reject(
+                    new Error(`Error downloading node: , ${error.message}`)
+                  );
+                });
+              });
+              response.pipe(writer);
+            }),
+        },
+        {
+          title: "Decompressing",
+          task: async (ctx): Promise<void> => {
+            try {
+              const archiveFilePath = path.resolve(
+                ctx.nodeTargetDir as string,
+                "node.zip"
+              );
+
+              const decompressed = await decompress(
+                archiveFilePath,
+                ctx.nodeTargetDir as string
+              );
+              ctx.nodeFileName = decompressed[0].path;
+              execSync(`rm -f ${archiveFilePath}`);
+              execSync(`chmod +x ${ctx.nodeTargetDir}/${ctx.nodeFileName}`);
+            } catch {}
+          },
+        },
+      ],
+      {
+        rendererOptions: { collapse: true },
+      }
+    );
 
     try {
-      await tasks.run();
-      //   const { contractTemplate } = await inquirer.prompt({
-      //     name: "contractTemplate",
-      //     type: "list",
-      //     message: "Which template should we use?",
-      //     choices: [
-      //       { name: "Blank", value: "master" },
-      //       { name: "Flipper", value: "flipper" },
-      //       { name: "Dual contract", value: "dual-contract" },
-      //     ],
-      //   });
-      //   await cloneTemplateRepo.run({ contractTemplate, name: args.name });
-      //   await downloadNode.run({
-      //     name: args.name,
-      //     platform: this.config.platform,
-      //   });
+      await tasks.run({ platform: this.config.platform, name: args.name });
     } catch {}
   }
 
@@ -64,124 +193,3 @@ export class Generate extends Command {
     console.error(_error);
   }
 }
-
-const checkDependencies = {
-  title: "Checking dependencies",
-  task: () => {
-    const tasks = Object.entries({
-      rust: "rustc --version",
-      cargo: "caro -V",
-      "cargo contract": "cargo contract -V",
-    }).map(([dependency, command]) => ({
-      title: `Checking ${dependency}`,
-      task: () => {
-        try {
-          execSync(command, { stdio: "ignore" });
-        } catch {
-          throw new Error(
-            `"${dependency}" is not installed. Please follow the guide: https://docs.substrate.io/tutorials/v3/ink-workshop/pt1/#update-your-rust-environment`
-          );
-        }
-      },
-    }));
-    return new Listr(tasks, { concurrent: true });
-  },
-};
-
-const cloneTemplateRepo = new Listr([
-  {
-    title: "Clone template repo",
-    task: (ctx) => {
-      execSync(
-        `git clone -b ${
-          ctx.contractTemplate
-        } --single-branch https://github.com/AstarNetwork/swanky-template-ink.git "${path.resolve(
-          ctx.name
-        )}"`,
-        { stdio: "ignore" }
-      );
-      rmSync(`${path.resolve(ctx.name, ".git")}`, { recursive: true });
-    },
-  },
-]);
-
-// const downloadNode = new Listr([
-//   {
-//     title: "Choose node type",
-//     task: (ctx) =>
-//       listrInquirer(
-//         [
-//           {
-//             name: "nodeType",
-//             type: "list",
-//             message: "What node type you want to develop on?",
-//             choices: [{ name: "Swanky node", value: "swanky" }],
-//           },
-//         ],
-//         function (answers: any) {
-//           const targetDir = path.resolve(ctx.name, "bin");
-//           if (!existsSync(targetDir)) {
-//             mkdirSync(targetDir);
-//           }
-
-//           ctx.nodeUrl = nodes[answers.nodeType][ctx.platform];
-//           ctx.targetDir = targetDir;
-//           console.log(ctx);
-//         }
-//       ),
-//   },
-//   {
-//     title: "Downloading",
-//     task: (ctx) =>
-//       new Promise<void>((resolve, reject) => {
-//         const writer = createWriteStream(
-//           path.resolve(ctx.targetDir, "node.zip")
-//         );
-
-//         const response = download(ctx.nodeUrl, { extract: true });
-
-//         response.on("response", (res) => {
-//           const contentLength = Number.parseInt(
-//             res.headers["content-length"] as unknown as string,
-//             10
-//           );
-//           const bar = new ProgressBar(
-//             "Downloading node: [:bar] :rate/bps :percent :etas",
-//             {
-//               complete: "=",
-//               incomplete: " ",
-//               width: 20,
-//               total: contentLength,
-//             }
-//           );
-//           response.on("data", (chunk) => {
-//             bar.tick(chunk.length);
-//           });
-//           response.on("end", () => {
-//             resolve();
-//           });
-//           response.on("error", (error) => {
-//             reject(new Error(`Error downloading node: , ${error.message}`));
-//           });
-//         });
-//         response.pipe(writer);
-//       }),
-//   },
-// ]);
-
-// async function _decompressNode(
-//   pathToFile: string,
-//   fileName: string
-// ): Promise<void> {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       const filePath = path.resolve(pathToFile, fileName);
-//       const decompressed = decompress(filePath, pathToFile);
-//       console.log(decompressed);
-//       execSync(`rm -f ${filePath}`);
-//       resolve();
-//     } catch {
-//       reject(new Error("Unable to extract node"));
-//     }
-//   });
-// }
