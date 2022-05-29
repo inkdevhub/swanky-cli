902d52474206155f6cae959b9b0d769485712f5d
codespool <4625220+codespool@users.noreply.github.com>
2022-05-29 23:59:19 +0300

    add listr task to compile command

--- src/commands/compile/index.ts
+++ src/commands/compile/index.ts
@@ -1,5 +1,6 @@
-import { Command, CliUx } from "@oclif/core";
+import { Command, Flags } from "@oclif/core";
 import { execSync } from "node:child_process";
+import { Listr } from "listr2";
 import path = require("node:path");
 import { access } from "node:fs/promises";
 import { constants, readdirSync } from "node:fs";
@@ -7,25 +8,38 @@ export class Compile extends Command {
   static description =
     "Compile the smart contract(s) in your contracts directory";
 
-  static flags = {};
+  static flags = {
+    verbose: Flags.boolean({
+      default: false,
+      char: "v",
+      description: "Display all compilation output",
+    }),
+  };
 
   static args = [];
 
   async run(): Promise<void> {
-    // const { args, flags } = await this.parse(Compile);
+    const { flags } = await this.parse(Compile);
     try {
       await access("swanky.config.json", constants.R_OK);
     } catch {
       throw new Error("No 'swanky.config.json' detected in current folder!");
     }
 
-    const contractList = readdirSync(path.resolve("contracts"));
+    const tasks = new Listr([
+      {
+        title: "Compiling contract",
+        task: () => {
+          const contractList = readdirSync(path.resolve("contracts"));
 
-    CliUx.ux.action.start("Compiling..");
-    execSync("cargo +nightly contract build", {
-      cwd: path.resolve("contracts", contractList[0]),
-      stdio: "ignore",
-    });
-    CliUx.ux.action.stop();
+          execSync("cargo +nightly contract build", {
+            cwd: path.resolve("contracts", contractList[0]),
+            stdio: flags.verbose ? "inherit" : "ignore",
+          });
+        },
+      },
+    ]);
+
+    tasks.run();
   }
 }
