ef6931905494faeff8668092191b8c87a9daaac9
codespool <4625220+codespool@users.noreply.github.com>
2022-08-02 09:51:35 +0300

    pr fix: remove mnemoninc logging

--- src/commands/account/list.ts
+++ src/commands/account/list.ts
@@ -5,33 +5,15 @@ import { ensureSwankyProject, getSwankyConfig } from "../../lib/command-utils";
 export class CreateAccount extends Command {
   static description = "List dev accounts stored in config";
   static aliases = [`account:ls`];
-  static flags = {
-    verbose: Flags.boolean({
-      char: "v",
-    }),
-  };
-
-  static args = [];
 
   async run(): Promise<void> {
     await ensureSwankyProject();
 
-    const { flags } = await this.parse(CreateAccount);
-
     const config = await getSwankyConfig();
     this.log(`${greenBright("✔")} Stored dev accounts:`);
-    if (flags.verbose) {
-      config.accounts.forEach((account) => {
-        this.log(
-          `\t${yellowBright("Alias: ")} ${account.alias.padEnd(
-            12
-          )} ${yellowBright("Mnemonic: ")} ${account.mnemonic}`
-        );
-      });
-    } else {
-      config.accounts.forEach((account) => {
-        this.log(`\t${yellowBright("Alias: ")} ${account.alias}`);
-      });
-    }
+
+    config.accounts.forEach((account) => {
+      this.log(`\t${yellowBright("Alias: ")} ${account.alias}`);
+    });
   }
 }
