b2c4efa3deec65242dff2a247c93afdb3daa149c
codespool <4625220+codespool@users.noreply.github.com>
2022-08-04 14:26:19 +0300

    copy over templates

---
+++ src/templates/contracts/psp22/src/lib.rs
@@ -0,0 +1,61 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+#[brush::contract]
+pub mod my_psp22 {
+    use brush::contracts::psp22::*;
+    use ink_prelude::string::String;
+    use ink_storage::traits::SpreadAllocate;
+
+    #[ink(storage)]
+    #[derive(Default, SpreadAllocate, PSP22Storage)]
+    pub struct MyPSP22 {
+        #[PSP22StorageField]
+        psp22: PSP22Data,
+        // fields for hater logic
+        hated_account: AccountId,
+    }
+
+    impl PSP22Transfer for MyPSP22 {
+        // Let's override method to reject transactions to bad account
+        fn _before_token_transfer(
+            &mut self,
+            _from: Option<&AccountId>,
+            to: Option<&AccountId>,
+            _amount: &Balance,
+        ) -> Result<(), PSP22Error> {
+            if to == Some(&self.hated_account) {
+                return Err(PSP22Error::Custom(String::from("I hate this account!")))
+            }
+            Ok(())
+        }
+    }
+
+    impl PSP22 for MyPSP22 {}
+
+    impl MyPSP22 {
+        #[ink(constructor)]
+        pub fn new(total_supply: Balance) -> Self {
+            ink_lang::codegen::initialize_contract(|instance: &mut MyPSP22| {
+                instance
+                    ._mint(instance.env().caller(), total_supply)
+                    .expect("Should mint");
+            })
+        }
+
+        #[ink(message)]
+        pub fn set_hated_account(&mut self, hated: AccountId) {
+            self.hated_account = hated;
+        }
+
+        #[ink(message)]
+        pub fn get_hated_account(&self) -> AccountId {
+            self.hated_account.clone()
+        }
+
+        #[ink(message)]
+        pub fn get_total_supply(&self) -> Balance {
+            self.total_supply()
+        }
+    }
+}
---
+++ src/templates/package.json.tpl
@@ -0,0 +1,15 @@
+{
+  "name": "swanky-template",
+  "version": "1.0.0",
+  "main": "index.js",
+  "author": "Astar Network",
+  "license": "MIT",
+  "scripts": {
+    "run-node": "swanky node start",
+    "compile": "swanky compile",
+    "deploy": "swanky deploy"
+  },
+  "dependencies": {
+    "@astar-network/swanky-cli": "0.1.4"
+  }
+}
