3d7980a0d5a616b1171171a0fccda71094dcc186
codespool <4625220+codespool@users.noreply.github.com>
2022-10-31 17:06:34 +0200

    temp disable call

--- src/commands/contract/call.ts
+++ src/commands/contract/call.ts
@@ -33,29 +33,27 @@ export class CallContract extends Command {
   static args = [];
 
   async run(): Promise<void> {
-    const { flags } = await this.parse(CallContract);
-
-    const config = await getSwankyConfig();
-
-    const contractInfo = config.contracts?.find(item => item.address == flags.address);
-    if (!contractInfo) {
-      throw Error("contract address not found in swanky config")
-    }
-    execSync(
-      `cargo contract call --contract ${
-        contractInfo?.address
-      } --message ${flags.message} ${flags.args ? "--args " + flags.args : ""} --suri //Alice --gas ${
-        flags.gas ?? "100000000000"
-      } --url ${resolveNetworkUrl(config, flags.network ?? "")} ${
-        flags.dry ? "--dry-run" : ""
-      }`,
-      {
-        stdio: "inherit",
-        cwd: path.resolve(
-          "contracts",
-          contractInfo?.name ?? ""
-        ),
-      }
-    );
+    // const { flags } = await this.parse(CallContract);
+    // const config = await getSwankyConfig();
+    // const contractInfo = config.contracts?.find(item => item.address == flags.address);
+    // if (!contractInfo) {
+    //   throw Error("contract address not found in swanky config")
+    // }
+    // execSync(
+    //   `cargo contract call --contract ${
+    //     contractInfo?.address
+    //   } --message ${flags.message} ${flags.args ? "--args " + flags.args : ""} --suri //Alice --gas ${
+    //     flags.gas ?? "100000000000"
+    //   } --url ${resolveNetworkUrl(config, flags.network ?? "")} ${
+    //     flags.dry ? "--dry-run" : ""
+    //   }`,
+    //   {
+    //     stdio: "inherit",
+    //     cwd: path.resolve(
+    //       "contracts",
+    //       contractInfo?.name ?? ""
+    //     ),
+    //   }
+    // );
   }
 }
