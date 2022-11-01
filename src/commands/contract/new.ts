132d6e2f5b0fcaa27c23803d04c0b31143438deb
codespool <4625220+codespool@users.noreply.github.com>
2022-11-01 12:16:38 +0200

    add language selection to contract new

--- src/commands/contract/new.ts
+++ src/commands/contract/new.ts
@@ -1,15 +1,11 @@
 import { Command, Flags } from "@oclif/core";
 import path = require("node:path");
-import * as fs from 'fs-extra';
-import { getTemplates } from "../init";
+import { pathExistsSync, writeJSON } from "fs-extra";
+import { getAllTemplateNames, getSwankyConfig, getTemplates } from "../../lib/command-utils";
 import { ensureSwankyProject } from "../../lib/command-utils";
-import { email, name, pickTemplate } from "../../lib/prompts";
+import { email, name, pickLanguage, pickTemplate } from "../../lib/prompts";
 import { Spinner } from "../../lib/spinner";
-import {
-  checkCliDependencies,
-  copyContractTemplateFiles,
-  processTemplates,
-} from "../../lib/tasks";
+import { checkCliDependencies, copyContractTemplateFiles, processTemplates } from "../../lib/tasks";
 import { paramCase, pascalCase, snakeCase } from "change-case";
 import execa = require("execa");
 import inquirer = require("inquirer");
@@ -19,33 +15,57 @@ export class NewContract extends Command {
 
   static flags = {
     template: Flags.string({
-      options: getTemplates().contractTemplatesList.map((template) => template.value),
+      options: getAllTemplateNames(),
+    }),
+    language: Flags.string({
+      options: ["ink", "ask"],
+      char: "l",
+      required: false,
     }),
     verbose: Flags.boolean({ char: "v" }),
-  }
+  };
 
   static args = [
     {
       name: "contractName",
       required: true,
-      description: "Name of new contract"
-    }
-  ]
+      description: "Name of new contract",
+    },
+  ];
 
   async run(): Promise<void> {
     await ensureSwankyProject();
 
+    const config = await getSwankyConfig();
+
     const projectPath = path.resolve();
     const { args, flags } = await this.parse(NewContract);
 
-    if (fs.pathExistsSync(path.join(projectPath, "contracts", args.contractName))) {
-      throw Error(`Contract folder '${args.contractName}' already exists`);
+    if (
+      pathExistsSync(path.join(projectPath, "contracts", args.contractName)) ||
+      config.contracts[args.name]
+    ) {
+      throw new Error(`Contract folder '${args.contractName}' already exists`);
     }
 
-    const templates = getTemplates();
+    const { contractLanguage } = flags.language
+      ? { contractLanguage: flags.language }
+      : await inquirer.prompt([pickLanguage()]);
+
+    const templates = getTemplates(contractLanguage);
+
+    const { contractTemplate } = flags.template
+      ? { contractTemplate: flags.template }
+      : await inquirer.prompt([pickTemplate(templates.contractTemplatesList)]);
+
+    // passing language and template by flags can result in a non-existing combination
+    if (!templates.contractTemplatesList.includes(contractTemplate)) {
+      this.error(
+        `Selected template [${contractLanguage}] does not exist for selected language [${contractLanguage}]`
+      );
+    }
 
     const questions = [
-      pickTemplate(templates.contractTemplatesList),
       name(
         "author",
         () => execa.commandSync("git config --get user.name").stdout,
@@ -63,7 +83,7 @@ export class NewContract extends Command {
     await spinner.runCommand(
       () =>
         copyContractTemplateFiles(
-          path.resolve(templates.contractTemplatesPath, answers.contractTemplate),
+          path.resolve(templates.contractTemplatesPath, contractTemplate),
           args.contractName,
           projectPath
         ),
@@ -79,9 +99,20 @@ export class NewContract extends Command {
           swanky_version: this.config.pjson.version,
           contract_name_snake: snakeCase(args.contractName),
           contract_name_pascal: pascalCase(args.contractName),
+          contract_language: contractLanguage,
         }),
-        "Processing contract templates"
-      );
+      "Processing contract templates"
+    );
+
+    await spinner.runCommand(async () => {
+      config.contracts[args.name] = {
+        name: args.name,
+        language: contractLanguage,
+        deployments: [],
+      };
+
+      await writeJSON(path.resolve("swanky.config.json"), config, { spaces: 2 });
+    }, "Writing config");
 
     this.log("😎 New contract successfully generated! 😎");
   }
