0faa458d863726c1857b175196bebed34b31ecd7
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2022-11-02 00:24:30 +0900

    runcommand error exits

--- src/lib/spinner.ts
+++ src/lib/spinner.ts
@@ -45,6 +45,7 @@ export class Spinner {
     } catch (error) {
       this.fail(failMessage || `Error ${runningMessage}`);
       if (this.verbose) console.error(error);
+      process.exit(1);
     }
   }
 
