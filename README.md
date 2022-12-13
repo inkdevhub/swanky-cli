33fc490c7f1b57331ead427a4aeeba6fafbdc7a7
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2022-12-13 17:09:18 +0900

    revert changes & added local development procedures

---
+++ README.md
@@ -0,0 +1,6 @@
+### How to run locally
+```
+yarn
+npx lerna run build
+./packages/cli/bin/dev -h
+```
--- packages/core/src/lib/command-utils.ts
+++ packages/core/src/lib/command-utils.ts
@@ -84,7 +84,7 @@ export async function copyArtifactsFor(
       await Promise.all([
         fs.copyFile(
           path.resolve("artifacts", `${contractName}.contract`),
-          `${buildData.artifactsPath}/${contractName}.contract`
+          `${buildData.artifactsPath}/${contractName}.wasm`
         ),
         fs.copyFile(
           path.resolve("artifacts", `${contractName}.json`),
