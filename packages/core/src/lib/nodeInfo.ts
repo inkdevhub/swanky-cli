2033aac46cae9209b5762fe9473e37a8b4c7a178
codespool <4625220+codespool@users.noreply.github.com>
2022-12-14 11:34:16 +0200

    update node version to v1.0.0-jp-workshop

--- packages/core/src/lib/nodeInfo.ts
+++ packages/core/src/lib/nodeInfo.ts
@@ -1,13 +1,13 @@
 export type nodeInfo = typeof swankyNode;
 
 export const swankyNode = {
-  version: "0.10.0",
-  polkadotPalletVersions: "polkadot-v0.9.27",
-  supportedInk: "v3.3.1",
+  version: "v1.0.0-jp-workshop",
+  polkadotPalletVersions: "polkadot-v0.9.33",
+  supportedInk: "v4.0.0-beta",
   downloadUrl: {
     darwin:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/v0.10.0/swanky-node-v0.10.0-macOS-x86_64.tar.gz",
+      "https://github.com/AstarNetwork/swanky-node/releases/download/untagged-202acd9f33256bf1a000/swanky-node-v1.0.0-jp-workshop-macOS-x86_64.tar.gz",
     linux:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/v0.10.0/swanky-node-v0.10.0-ubuntu-x86_64.tar.gz",
+      "https://github.com/AstarNetwork/swanky-node/releases/download/untagged-202acd9f33256bf1a000/swanky-node-v1.0.0-jp-workshop-ubuntu-x86_64.tar.gz",
   },
 };
