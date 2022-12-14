3ab6369d8c46ba786682c565d1adcd48a77f7770
codespool <4625220+codespool@users.noreply.github.com>
2022-12-14 15:31:58 +0200

    update node urls

--- packages/core/src/lib/nodeInfo.ts
+++ packages/core/src/lib/nodeInfo.ts
@@ -6,8 +6,8 @@ export const swankyNode = {
   supportedInk: "v4.0.0-beta",
   downloadUrl: {
     darwin:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/untagged-202acd9f33256bf1a000/swanky-node-v1.0.0-jp-workshop-macOS-x86_64.tar.gz",
+      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.0.0-jp-workshop/swanky-node-v1.0.0-jp-workshop-macOS-x86_64.tar.gz",
     linux:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/untagged-202acd9f33256bf1a000/swanky-node-v1.0.0-jp-workshop-ubuntu-x86_64.tar.gz",
+      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.0.0-jp-workshop/swanky-node-v1.0.0-jp-workshop-ubuntu-x86_64.tar.gz",
   },
 };
