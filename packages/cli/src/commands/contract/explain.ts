09cfa063b3c35c582191ac6595d8dda0d7c28025
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2023-02-15 12:51:13 +0900

    contract explain command (#98)
    Simple implementation of contract explain command using `console.log`s.
    Better using templating and colonization for more visibility.
    
    <img width="1512" alt="Screen Shot 2023-02-10 at 16 35 47"
    src="https://user-images.githubusercontent.com/28953860/218030686-09ad4a0a-6ba2-4bc1-8b0d-afae5e4164ee.png">

---
+++ packages/cli/src/commands/contract/explain.ts
@@ -0,0 +1,45 @@
+import { BaseCommand } from "../../lib/baseCommand";
+import * as fs from "fs-extra";
+import path = require("node:path");
+import { readdirSync } from "node:fs";
+import { printContractInfo } from "@astar-network/swanky-core";
+
+export class ExplainContract extends BaseCommand<typeof ExplainContract> {
+  static description = "Explain contract messages based on thier metadata";
+
+  static args = [
+    {
+      name: "contractName",
+      required: true,
+      description: "Name of the contract",
+    },
+  ];
+
+  async run(): Promise<void> {
+    const { args } = await this.parse(ExplainContract);
+
+    const contractInfo = this.swankyConfig.contracts[args.contractName];
+    if (!contractInfo) {
+      this.error(`Cannot find a contract named ${args.contractName} in swanky.config.json`);
+    }
+
+    const contractList = readdirSync(path.resolve("contracts"));
+
+    const contractPath = path.resolve("contracts", args.contractName);
+    if (!contractList.includes(args.contractName)) {
+      this.error(`Path to contract ${args.contractName} does not exist: ${contractPath}`);
+    }
+
+    if (!contractInfo.build) {
+      this.error(`No build data for contract "${args.contractName}"`);
+    }
+    
+    const metadataPath = path.resolve(contractInfo.build?.artifactsPath, `${args.contractName}.json`);
+    if (!fs.existsSync(metadataPath)) {
+        this.error(`Metadata json file for ${args.contractName} contract not found`);
+    }
+
+    printContractInfo(metadataPath)
+  }
+}
+
