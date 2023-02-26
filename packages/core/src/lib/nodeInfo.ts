75611068beae0a6758f3dfb8cf79db966b15029c
HyunggyuJang <murasakipurplez5@gmail.com>
2023-02-26 22:11:30 +0900

    Uplift to ink v4 (#117)
    Now [cargo contract](https://github.com/paritytech/cargo-contract)
    released the stable version of v2.0.1, and it only supports from ink v4.
    Closes #105. Also partly handles #109 (the ink version part).
    
    ---------
    
    Co-authored-by: keisukefunatsu <keisukeyan2012@gmail.com>

--- packages/core/src/lib/nodeInfo.ts
+++ packages/core/src/lib/nodeInfo.ts
@@ -1,13 +1,13 @@
 export type nodeInfo = typeof swankyNode;
 
 export const swankyNode = {
-  version: "1.0.0",
-  polkadotPalletVersions: "polkadot-v0.9.33",
+  version: "1.1.0",
+  polkadotPalletVersions: "polkadot-v0.9.37",
   supportedInk: "v4.0.0",
   downloadUrl: {
     darwin:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.0.0/swanky-node-v1.0.0-macOS-x86_64.tar.gz",
+      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-macOS-x86_64.tar.gz",
     linux:
-      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.0.0/swanky-node-v1.0.0-ubuntu-x86_64.tar.gz",
+      "https://github.com/AstarNetwork/swanky-node/releases/download/v1.1.0/swanky-node-v1.1.0-ubuntu-x86_64.tar.gz",
   },
 };
