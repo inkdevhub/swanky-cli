f0f1c48e52019133c3a86c9ab556295bda6641d8
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2023-03-01 12:27:28 +0900

    compile all contracts if contract name not specified

--- packages/cli/src/commands/contract/compile.ts
+++ packages/cli/src/commands/contract/compile.ts
@@ -31,8 +31,9 @@ export class CompileContract extends Command {
   static args = [
     {
       name: "contractName",
-      required: true,
-      description: "Name of the contract to compile",
+      required: false,
+      default: "",
+      description: "Name of the contract to compile, compile all contracts if not specified.",
     },
   ];
 
@@ -43,64 +44,70 @@ export class CompileContract extends Command {
 
     const config = await getSwankyConfig();
 
-    const contractInfo = config.contracts[args.contractName];
-    if (!contractInfo) {
-      this.error(`Cannot find a contract named ${args.contractName} in swanky.config.json`);
+    let contractList = readdirSync(path.resolve("contracts"));
+    if (args.contractName !== "") {
+      if (!config.contracts[args.contractName]) {
+        this.error(`Cannot find a contract named ${args.contractName} in swanky.config.json`);
+      }
+
+      if (!contractList.includes(args.contractName)) {
+        this.error(`Path to contract ${args.contractName} does not exist: ${path.resolve("contracts", args.contractName)}`);
+      }
+
+      // remove unselected contracts from contractList to compile
+      contractList = contractList.filter(contractName => contractName === args.contractName);
+    } else {
+      if (contractList.length === 0) {
+        this.error("Nothing to compile");
+      }
+      for (const contractName of contractList) {
+        console.log(`${contractName} contract is found`)
+      }
     }
 
     const spinner = new Spinner();
 
-    const contractList = readdirSync(path.resolve("contracts"));
+    for (const contractName of contractList) {
+      const contractInfo = config.contracts[contractName];
+      const contractPath = path.resolve("contracts", contractName);
 
-    const contractPath = path.resolve("contracts", args.contractName);
-    if (!contractList.includes(args.contractName)) {
-      this.error(`Path to contract ${args.contractName} does not exist: ${contractPath}`);
+      await spinner.runCommand(
+        async () => {
+          return new Promise<void>((resolve, reject) => {
+            const compile = getBuildCommandFor(contractInfo.language, contractPath, flags.release);
+            compile.stdout.on("data", () => spinner.ora.clear());
+            compile.stdout.pipe(process.stdout);
+            if (flags.verbose) {
+              compile.stderr.on("data", () => spinner.ora.clear());
+              compile.stderr.pipe(process.stdout);
+            }
+            compile.on("exit", (code) => {
+              if (code === 0) resolve();
+              else reject();
+            });
+          });
+        },
+        `Compiling ${contractName} contract`,
+        `${contractName} Contract compiled successfully`,
+      );
+
+      await spinner.runCommand(
+        async () => await generateTypesFor(contractInfo.language, contractInfo.name, contractPath),
+        `Generating ${contractName} contract ts types`,
+        `${contractName} contract's TS types Generated successfully`
+      );
+
+      const buildData = (await spinner.runCommand(async () => {
+        return moveArtifacts(contractInfo.name);
+      }, "Moving artifacts")) as BuildData;
+
+      contractInfo.build = buildData;
     }
 
-    await spinner.runCommand(
-      async () => {
-        return new Promise<void>((resolve, reject) => {
-          const compile = getBuildCommandFor(contractInfo.language, contractPath, flags.release);
-          compile.stdout.on("data", () => spinner.ora.clear());
-          compile.stdout.pipe(process.stdout);
-          if (flags.verbose) {
-            compile.stderr.on("data", () => spinner.ora.clear());
-            compile.stderr.pipe(process.stdout);
-          }
-          compile.on("exit", (code) => {
-            if (code === 0) resolve();
-            else reject();
-          });
-        });
-      },
-      "Compiling contract",
-      "Contract compiled successfully",
-    );
-
-    await spinner.runCommand(
-      async () => await generateTypesFor(contractInfo.language, contractInfo.name, contractPath),
-      "Generating contract ts types",
-      "TS types Generated successfully"
-    );
-
-    const buildData = (await spinner.runCommand(async () => {
-      return moveArtifacts(contractInfo.name);
-    }, "Moving artifacts")) as BuildData;
-
     await spinner.runCommand(async () => {
-      contractInfo.build = buildData;
-
       await writeJSON(path.resolve("swanky.config.json"), config, {
         spaces: 2,
       });
     }, "Writing config");
   }
-}
-
-// https://github.com/Supercolony-net/typechain-polkadot#usage-of-typechain-compiler
-interface TypechainCompilerConfig {
-  projectFiles: string[]; // Path to all project files, everystring in glob format
-  skipLinting : boolean; // Skip linting of project files
-  artifactsPath : string; // Path to artifacts folder, where artifacts will be stored it will save both .contract and .json (contract ABI)
-  typechainGeneratedPath : string; // Path to typechain generated folder
 }
