bb10178dc99b1efb4ef695ae5fb6b8f541dcc08b
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2023-03-13 12:35:12 +0900

    compile all contracts if contract name not specified (#121)
    Resolve https://github.com/AstarNetwork/swanky-cli/issues/110

--- packages/cli/src/commands/contract/test.ts
+++ packages/cli/src/commands/contract/test.ts
@@ -1,11 +1,10 @@
 require("ts-mocha");
-import { Args, Command } from "@oclif/core";
+import { Command, Flags, Args } from "@oclif/core";
 import path = require("node:path");
-import { readdirSync } from "node:fs";
 import { ensureSwankyProject, getSwankyConfig } from "@astar-network/swanky-core";
 import globby from "globby";
 import Mocha from "mocha";
-import { ensureDir } from "fs-extra";
+import { ensureDir, readdirSync } from "fs-extra";
 import * as shell from "shelljs";
 
 declare global {
@@ -18,6 +17,14 @@ export class CompileContract extends Command {
   // hidden until the mocha loading issue is resolved
   static hidden = true;
 
+  static flags = {
+    all: Flags.boolean({
+      default: false,
+      char: "a",
+      description: "Set all to true to compile all contracts"
+    })
+  };
+
   static args = {
     contractName: Args.string({
       name: "contractName",
@@ -27,60 +34,76 @@ export class CompileContract extends Command {
   };
 
   async run(): Promise<void> {
-    const { args } = await this.parse(CompileContract);
+    const { args, flags } = await this.parse(CompileContract);
+
+    if (args.contractName == "" && !flags.all) {
+      this.error("No contracts were selected to compile")
+    }
 
     await ensureSwankyProject();
-
     const config = await getSwankyConfig();
 
-    const contractInfo = config.contracts[args.contractName];
-    if (!contractInfo) {
-      this.error(`Cannot find a contract named ${args.contractName} in swanky.config.json`);
-    }
-
-    const contractList = readdirSync(path.resolve("contracts"));
-
-    const contractPath = path.resolve("contracts", args.contractName);
-    if (!contractList.includes(args.contractName)) {
-      this.error(`Path to contract ${args.contractName} does not exist: ${contractPath}`);
-    }
-
-    if (!contractInfo.build) {
-      this.error(`Cannot find build data for ${args.contractName} contract in swanky.config.json`);
-    }
-    const buildData = contractInfo.build;
-
-    const reportDir = path.resolve(buildData.artifactsPath, "testReports", Date.now().toString());
-    await ensureDir(reportDir);
-
-    const mocha = new Mocha({
-      timeout: 200000,
-      reporter: "mochawesome",
-      reporterOptions: {
-        reportDir,
-        charts: true,
-        reportTitle: `${args.contractName} test report`,
-        quiet: true,
-        json: false,
-      },
-    });
-
-    const tests = await globby(`${path.resolve("test", args.contractName)}/*.test.ts`);
-
-    mocha.addFile;
-    tests.forEach((test) => {
-      mocha.addFile(test);
-    });
-
-    global.contractTypesPath = path.resolve("test", args.contractName, "typedContract");
-
-    shell.cd(`./test/${args.contractName}`);
-    mocha.run((failures) => {
-      if (failures) {
-        this.error(`At least one of the tests failed. Check report for details: ${reportDir}`);
-      } else {
-        this.log(`All tests passing. Check the report for details: ${reportDir}`);
+    const contractNames = [];
+    if (flags.all) {
+      const contractList = readdirSync(path.resolve("contracts"), { withFileTypes: true });
+      for (const contract of contractList) {
+        if (contract.isDirectory()) {
+          console.log(`${contract.name} contract is found`);
+          contractNames.push(contract.name);
+        }
       }
-    });
+    } else {
+      contractNames.push(args.contractName);
+    }
+
+    const projectDir = path.resolve();
+    const testDir = path.resolve("test");
+    for (const contractName of contractNames) {
+      const contractInfo = config.contracts[contractName];
+      if (!contractInfo.build) {
+        this.error(`Cannot find build data for ${contractName} contract in swanky.config.json`);
+      }
+      const buildData = contractInfo.build;
+
+      const reportDir = path.resolve(projectDir, buildData.artifactsPath, "testReports", Date.now().toString());
+      await ensureDir(reportDir);
+  
+      const mocha = new Mocha({
+        timeout: 200000,
+        reporter: "mochawesome",
+        reporterOptions: {
+          reportDir,
+          charts: true,
+          reportTitle: `${contractName} test report`,
+          quiet: true,
+          json: false,
+        },
+      });
+  
+      const tests = await globby(`${path.resolve(testDir, contractName)}/*.test.ts`);
+  
+      mocha.addFile;
+      tests.forEach((test) => {
+        mocha.addFile(test);
+      });
+  
+      global.contractTypesPath = path.resolve(testDir, contractName, "typedContract");
+  
+      shell.cd(`${testDir}/${contractName}`);
+      try {
+        await new Promise<void>((resolve, reject) => {
+          mocha.run((failures) => {
+            if (failures) {
+              reject(`At least one of the tests failed. Check report for details: ${reportDir}`);
+            } else {
+              this.log(`All tests passing. Check the report for details: ${reportDir}`);
+              resolve();
+            }
+          });
+        });
+      } catch (error) {
+        this.error(error as string);
+      }
+    }
   }
 }
