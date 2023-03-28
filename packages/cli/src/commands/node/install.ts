1274127dda4b334b5ceb28a1c0b72209b4f45853
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2023-03-28 13:32:44 +0900

    added install swanky node command

---
+++ packages/cli/src/commands/node/install.ts
@@ -0,0 +1,52 @@
+import { Command, Flags } from "@oclif/core";
+import { ensureSwankyProject, getSwankyConfig, Spinner, downloadNode, swankyNode } from "@astar-network/swanky-core";
+import inquirer = require("inquirer");
+import path = require("node:path");
+import { writeJSON } from "fs-extra";
+import { choice } from "../../lib/prompts";
+
+export class InstallNode extends Command {
+  static description = "Install swanky node binary";
+
+  static flags = {
+    verbose: Flags.boolean({ char: "v" }),
+  };
+
+  async run(): Promise<void> {
+    ensureSwankyProject();
+
+    const { flags } = await this.parse(InstallNode);
+    const config = await getSwankyConfig();
+    const spinner = new Spinner(flags.verbose);
+
+    const projectPath = path.resolve();
+
+    if (config.node.localPath !== "") {
+        const answers = await inquirer.prompt([
+            choice("overwrite", "Swanky node already installed. Do you want to overwrite it?")
+        ]);
+        if (!answers.overwrite) {
+            return;
+        }
+    }
+
+    const taskResult = (await spinner.runCommand(
+        () => downloadNode(projectPath, swankyNode, spinner),
+        "Downloading Swanky node"
+    )) as string;
+    const nodePath = path.relative(projectPath, taskResult);
+
+    config.node = {
+        localPath: nodePath,
+        polkadotPalletVersions: swankyNode.polkadotPalletVersions,
+        supportedInk: swankyNode.supportedInk,
+    }
+
+    await spinner.runCommand(
+        () => writeJSON(path.resolve(projectPath, "swanky.config.json"), config, { spaces: 2 }),
+        "Updating swanky config"
+    );
+
+    this.log("Swanky Node Installed successfully");
+  }
+}
