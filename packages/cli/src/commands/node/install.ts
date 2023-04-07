eca15e537dcae8914bfce0ee58b5844aac93790e
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2023-04-07 12:53:01 +0900

    resolve comments

--- packages/cli/src/commands/node/install.ts
+++ packages/cli/src/commands/node/install.ts
@@ -1,6 +1,6 @@
 import { BaseCommand } from "../../lib/baseCommand";
 import { ux } from "@oclif/core";
-import { ensureSwankyProject, getSwankyConfig, Spinner, downloadNode, swankyNode } from "@astar-network/swanky-core";
+import { ensureSwankyProject, downloadNode, swankyNode } from "@astar-network/swanky-core";
 import path = require("node:path");
 import { writeJSON } from "fs-extra";
 
@@ -10,33 +10,29 @@ export class InstallNode extends BaseCommand<typeof InstallNode> {
   async run(): Promise<void> {
     await ensureSwankyProject();
 
-    const { flags } = await this.parse(InstallNode);
-    const config = await getSwankyConfig();
-    const spinner = new Spinner(flags.verbose);
-
     const projectPath = path.resolve();
 
-    if (config.node.localPath !== "") {
+    if (this.swankyConfig.node.localPath !== "") {
         const overwrite = await ux.confirm("Swanky node already installed. Do you want to overwrite it? (y/n)");
         if (!overwrite) {
             return;
         }
     }
 
-    const taskResult = (await spinner.runCommand(
-        () => downloadNode(projectPath, swankyNode, spinner),
+    const taskResult = (await this.spinner.runCommand(
+        () => downloadNode(projectPath, swankyNode, this.spinner),
         "Downloading Swanky node"
     )) as string;
     const nodePath = path.relative(projectPath, taskResult);
 
-    config.node = {
+    this.swankyConfig.node = {
         localPath: nodePath,
         polkadotPalletVersions: swankyNode.polkadotPalletVersions,
         supportedInk: swankyNode.supportedInk,
     }
 
-    await spinner.runCommand(
-        () => writeJSON(path.resolve(projectPath, "swanky.config.json"), config, { spaces: 2 }),
+    await this.spinner.runCommand(
+        () => writeJSON(path.resolve(projectPath, "swanky.config.json"), this.swankyConfig, { spaces: 2 }),
         "Updating swanky config"
     );
 
