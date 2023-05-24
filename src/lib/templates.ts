fd7796336051b91ca0b8394fd7fd655fa0362bb3
codespool <4625220+codespool@users.noreply.github.com>
2023-05-24 21:19:06 +0300

    update __dirname syntax

--- src/lib/templates.ts
+++ src/lib/templates.ts
@@ -1,6 +1,9 @@
 import { readdirSync } from "fs";
+import { fileURLToPath } from "url";
 import path from "node:path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export function getTemplates() {
   const templatesPath = path.resolve(__dirname, "templates");
   const contractTemplatesPath = path.resolve(templatesPath, "contracts");
