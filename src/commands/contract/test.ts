5e5a89748c5e211d5048b427a3b7b3ccf2a438b3
codespool <4625220+codespool@users.noreply.github.com>
2023-05-25 17:03:44 +0300

    fix shelljs import

--- src/commands/contract/test.ts
+++ src/commands/contract/test.ts
@@ -5,7 +5,7 @@ import { ensureSwankyProject, getSwankyConfig } from "../../lib/index.js";
 import { globby } from "globby";
 import Mocha from "mocha";
 import { emptyDir } from "fs-extra/esm";
-import * as shell from "shelljs";
+import shell from "shelljs";
 import { Contract } from "../../lib/contract.js";
 
 declare global {
