c8a8e47a9980e46981e3121287c627fee7dcc94a
codespool <4625220+codespool@users.noreply.github.com>
2023-05-31 10:45:12 +0300

    chore: remove redundant dev.js script

--- bin/dev.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ts-node
-
-import "@polkadot/api-augment";
-
-const oclif = await import("@oclif/core");
-await oclif.execute({ type: "esm", development: true, dir: import.meta.url });
