0c99296a534babf483fcd2a6efc56fef97359122
PierreOssun <35110271+PierreOssun@users.noreply.github.com>
2023-10-10 15:59:43 +0200

    Update consts.ts
    https://evm.shibuya.astar.network (only EVM/Ethereum RPC available) so updating to something compatible with Wasm contracts

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_NETWORK_URL = "ws://127.0.0.1:9944";
 export const DEFAULT_ASTAR_NETWORK_URL = "wss://rpc.astar.network";
 export const DEFAULT_SHIDEN_NETWORK_URL = "wss://rpc.shiden.astar.network";
-export const DEFAULT_SHIBUYA_NETWORK_URL = "wss://rpc.shibuya.astar.network";
+export const DEFAULT_SHIBUYA_NETWORK_URL = "https://shibuya.public.blastapi.io";
 
 export const ARTIFACTS_PATH = "artifacts";
 export const TYPED_CONTRACTS_PATH = "typedContracts";
