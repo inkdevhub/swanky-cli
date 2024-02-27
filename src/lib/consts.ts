ac1a002e6bfbd153330489466671563015017fbd
Igor Papandinas <igor.papandinas@posteo.net>
2024-02-27 17:29:30 +0100

    chore: Bump dev env deps

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -6,7 +6,7 @@ export const DEFAULT_SHIBUYA_NETWORK_URL = "wss://shibuya.public.blastapi.io";
 export const ARTIFACTS_PATH = "artifacts";
 export const TYPED_CONTRACTS_PATH = "typedContracts";
 
-export const DEFAULT_RUST_DEP_VERSION = "1.73.0";
-export const DEFAULT_RUST_NIGHTLY_DEP_VERSION = "nightly-2023-03-05";
-export const DEFAULT_CARGO_DYLINT_DEP_VERSION = "2.1.11";
-export const DEFAULT_CARGO_CONTRACT_DEP_VERSION = "4.0.0-alpha";
+export const DEFAULT_RUST_DEP_VERSION = "1.76.0";
+export const DEFAULT_RUST_NIGHTLY_DEP_VERSION = "nightly-2024-05-02";
+export const DEFAULT_CARGO_DYLINT_DEP_VERSION = "2.6.1";
+export const DEFAULT_CARGO_CONTRACT_DEP_VERSION = "4.0.0-rc.2";
