ec0cfb3a7f94bd6d35dc285ae0503e8d5266cf78
Olexandr Berezan <116814585+prxgr4mm3r@users.noreply.github.com>
2024-03-12 17:20:01 +0200

    feat: Add Chopsticks integration
    This PR are connected to [Ganache-like testing feature
    #49](https://github.com/inkdevhub/swanky-node/issues/49). The main
    purpose of this issue is to make testing easier by implementing next
    features:
    > * block time stamp manipulation (can mine a block with arbitrary block
    time stamp)
    > * block number manipulation
    > * taking a snapshot and retrieve later
    > * Balance manipulation of accounts
    > * Impersonate any account
    
    All of them can already implemented in
    [Chopsticks](https://github.com/AcalaNetwork/chopsticks). Below I will
    explain how to use it.
    Firstly, you should set up your swanky project with `swanky init` and
    choose that you want to install swanky-node. Then you should use `swanky
    node chopsticks init` to initiate chopsticks config, which will be
    stored in the `./node/config/` folder. Then you start the node(`swanky
    node start`) and fork it with chopsticks(`swanky node chopsticks
    start`).
    Now we have a testing node that has all the features that we need.
    Chopsticks allow to use of different RPC calls. Full list of them you
    can check
    [here](https://acalanetwork.github.io/chopsticks/docs/chopsticks/README.html).
    For example, you can do timestamp manipulation by `dev_timeTravel' RPC
    call:
    ```TypeScript
    import { WsProvider } from '@polkadot/rpc-provider'
    const ws = new WsProvider(`ws://localhost:8000`)
    await ws.send('dev_timeTravel', ['Jan 1, 2023'])
    ```
    Or go back to any block by 'dev_setHead':
    ```TypeScript
    import { WsProvider } from '@polkadot/rpc-provider'
    const ws = new WsProvider(`ws://localhost:8000`)
    await ws.send('dev_setHead', [1000000])
    ```
    
    ---------
    
    Co-authored-by: Igor Papandinas <igor.papandinas@posteo.net>

---
+++ src/commands/node/chopsticks/init.ts
@@ -0,0 +1,39 @@
+import path from "node:path";
+import { SwankyCommand } from "../../../lib/swankyCommand.js";
+import {
+  copyChopsticksTemplateFile,
+  getSwankyConfig,
+  getTemplates,
+} from "../../../lib/index.js";
+import { ConfigBuilder } from "../../../lib/config-builder.js";
+import { SwankyConfig } from "../../../types/index.js";
+
+export const chopsticksConfig = "dev.yml";
+
+export class InitChopsticks extends SwankyCommand<typeof InitChopsticks> {
+  static description = "Initialize chopsticks config";
+
+  async run(): Promise<void> {
+    const localConfig = getSwankyConfig("local") as SwankyConfig;
+    const projectPath = path.resolve();
+
+    const chopsticksTemplatePath = getTemplates().chopsticksTemplatesPath;
+    const configPath = path.resolve(projectPath, "node", "config");
+
+    await this.spinner.runCommand(
+      () =>
+        copyChopsticksTemplateFile(chopsticksTemplatePath, configPath),
+      "Copying Chopsticks template files...",
+    );
+
+    await this.spinner.runCommand(async () => {
+      const newLocalConfig = new ConfigBuilder(localConfig)
+        .addChopsticks(path.resolve(projectPath, "node", "config", chopsticksConfig))
+        .build();
+      await this.storeConfig(newLocalConfig, "local");
+    }, "Updating Swanky configuration with Chopsticks settings...");
+
+    this.log("Chopsticks config initialized successfully");
+  }
+}
+
